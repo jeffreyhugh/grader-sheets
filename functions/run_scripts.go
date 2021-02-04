cd94400dc9de22850f63c4c44aff0e3849815372
qbxt <10813JJ@gmail.com>
2021-02-04 14:20:23 -0700

    fix scripts

--- functions/run_scripts.go
+++ functions/run_scripts.go
@@ -16,9 +16,9 @@ func RunScripts(SubmissionsDirectory string) error {
 		cmd := exec.Command("./scripts/" + script.Name(), SubmissionsDirectory)
 		output, err := cmd.Output()
 		if err != nil {
-			fmt.Printf("%s", err.Error())
+			fmt.Printf("    %s", err.Error())
 		}
-		fmt.Printf("%s\n", output)
+		fmt.Printf("    %s\n", output)
 		fmt.Printf("  %s exited with code %d\n", script.Name(), cmd.ProcessState.ExitCode())
 	}
 
